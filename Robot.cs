2edea87bab3cab4dfe2d4088361145b07cb964a7
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2020-09-24 21:43:04 -0400

    Added beliefs, objectives and intentions to robot.

--- Robot.cs
+++ Robot.cs
@@ -20,22 +20,29 @@ namespace AspirobotT01
         public string ImagePath { get; set; }
         public RobotDisplay robotDisplay { get; set; }
 
-        private Place place;
+        private Place realTimeSensorPlace;
+        private Place internalCurrentPlaceState;
+
         private int positionWhereRobotIs = 0;
         private int electricity = 0;
         private int countDirtyCollected = 0;
         private int countJewelCollected = 0;
         private int countPenitenceReceived = 0;
-        private Actions chosedAction;
 
         private enum Actions
         {
-            Move,
+            Explore,
             Aspire,
-            Collect
+            Collect,
+            MoveLeft,
+            MoveRight,
+            MoveUp,
+            MoveDown
         }
 
-        //private Random random = new Random();
+        private List<Actions> intentions = new List<Actions>();
+
+        private List<List<Actions>> beliefs = new List<List<Actions>>();
 
         public Robot()
         {
@@ -47,40 +54,81 @@ namespace AspirobotT01
 
         private void robotSensor_OnEnvironmentChange(List<Place> places)
         {
-            place = places[positionWhereRobotIs];
+            realTimeSensorPlace = places[positionWhereRobotIs];
         }
 
         internal void Execute()
         {
             Thread.Sleep(333);
 
-            //ObserveEnvironmentWithAllMySensors();
-            //UpdateMyState();
+            ObserveEnvironmentWithAllMySensors();
+            UpdateMyState();
             ChooseAnAction();
             JustDoIt();
         }
 
+        private void ObserveEnvironmentWithAllMySensors()
+        {
+            if (intentions.Count() > 0)
+                return;
+
+            internalCurrentPlaceState = realTimeSensorPlace;
+        }
+
+        private void UpdateMyState()
+        {
+            if (internalCurrentPlaceState == null)
+                return;
+
+            if (intentions.Count() > 0)
+                return;
+
+            beliefs.Clear();
+
+            if (internalCurrentPlaceState.jewel == null && internalCurrentPlaceState.dirty == null)
+                beliefs.Add(new List<Actions>() { Actions.Explore });
+            else if (internalCurrentPlaceState.jewel != null && internalCurrentPlaceState.dirty == null)
+                beliefs.Add(new List<Actions>() { Actions.Collect });
+            else if (internalCurrentPlaceState.jewel == null && internalCurrentPlaceState.dirty != null)
+                beliefs.Add(new List<Actions>() { Actions.Aspire });
+            else if (internalCurrentPlaceState.jewel != null && internalCurrentPlaceState.dirty != null)
+                beliefs.Add(new List<Actions>() { Actions.Collect, Actions.Aspire });
+        }
+
         private void ChooseAnAction()
         {
-            switch (chosedAction)
+            if (intentions.Count() > 0)
+                return;
+
+            foreach (List<Actions> belief in beliefs)
             {
-                case Actions.Move:
-                    chosedAction = Actions.Aspire;
-                    break;
-                case Actions.Aspire:
-                    chosedAction = Actions.Collect;
-                    break;
-                case Actions.Collect:
-                    chosedAction = Actions.Move;
-                    break;
+                //CleanEntireEnvironment,
+                if (belief.Last() == Actions.Explore)
+                    intentions = Explore();
+
+                //CleanCurrentPlace
+                if (belief.Last() == Actions.Aspire)
+                    intentions = belief;
+
+                if (belief.Last() == Actions.Collect)
+                    intentions = belief;
             }
+        }
+
+        private List<Actions> Explore()
+        {
+            return new List<Actions>() { Actions.MoveUp };
+            //TODO will change
         }
 
         private void JustDoIt()
         {
-            switch (chosedAction)
+            if (intentions.Count() == 0)
+                return;
+
+            switch (intentions.First())
             {
-                case Actions.Move:
+                case Actions.MoveUp:
                     //TODO: this logic will be changed for exploration, after
                     positionWhereRobotIs++;
 
@@ -88,6 +136,12 @@ namespace AspirobotT01
                         positionWhereRobotIs = 0;
 
                     RaiseMoveRobot(this, positionWhereRobotIs);
+                    break;
+                case Actions.MoveDown:
+                    break;
+                case Actions.MoveLeft:
+                    break;
+                case Actions.MoveRight:
                     break;
                 case Actions.Aspire:
                     countDirtyCollected++;
@@ -99,6 +153,8 @@ namespace AspirobotT01
                     RaiseCollectRobot(positionWhereRobotIs);
                     break;
             }
+
+            intentions.RemoveAt(0);
 
             electricity++;
 
